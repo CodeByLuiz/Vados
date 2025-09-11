ce1587a2b9769755722c83dfd0c6d775d4d7b2b8
Pedro119 <pedropelegrino19@gmail.com>
2025-09-11 09:14:06 -0300

    agora ta certo

--- Vados/BancoDeDados.cs
+++ Vados/BancoDeDados.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Vados
 {
     internal class BancoDeDados
     {
+        public class HistoryEntry
+        {
+            public required DateTime data { get; set; }
+            public required int id { get; set; }   
+            public required string comando { get; set; }
+            public required List<string> pastas { get; set; }
+
+        }
+
+        public class DbConnection : DbContext
+        {
+            public DbSet<HistoryEntry> Historico { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=historico.db");
+            }
+
+        }
+
     }
 }

299d4914c9fdf71a026aa68748e3ccd5bcfaf325
Pedro <pedropelegrino19@gmail.com>
2025-09-24 11:59:16 -0300

    ajustes no design dos botões

--- Vados/FormHistory.cs
+++ Vados/FormHistory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -29,23 +30,16 @@ namespace Vados
         {
             InitializeComponent();
 
-            this.DoubleBuffered = true;
-            this.ResizeRedraw = true;
-            SetStyle(ControlStyles.OptimizedDoubleBuffer |
-                     ControlStyles.UserPaint |
-                     ControlStyles.AllPaintingInWmPaint, true);
-
-            this.Text = "Histórico de Comandos";
-            this.Size = new Size(500, 700);
-            this.BackColor = Color.White;
-
-            // Painel com rolagem automática
+            
             historyPanel = new Panel
             {
                 Dock = DockStyle.Fill,
                 AutoScroll = true,
                 Padding = new Padding(20),
-                BackColor = Color.White
+                BackColor = Color.White,
+                
+
+
             };
             this.Controls.Add(historyPanel);
 
@@ -66,16 +60,16 @@ namespace Vados
             {
                 db.Database.EnsureCreated();
                 entradas = db.Historico
-                             .OrderBy(e => e.Data)
+                             .OrderByDescending(e => e.Data)
                              .ToList();
             }
 
             int margin = 10;
-            int startY = 10;
+            
             int rectangleHeight = 75;
             int rectangleWidth = historyPanel.ClientSize.Width - 40;
-
-           
+            int spacing = ((historyPanel.ClientSize.Width- rectangleWidth)/2);
+            int startY = spacing ;
 
             foreach (var entry in entradas)
             {
@@ -88,28 +82,35 @@ namespace Vados
 
                     Size = new Size(rectangleWidth, rectangleHeight),
                     BackColor = Color.LightGray,
-                    BorderStyle = BorderStyle.FixedSingle,
-                    Padding = new Padding(5),  // Leve padding para melhorar o visual
+                    BorderStyle = BorderStyle.None,
+                    
+                    Padding = new Padding(5),  
+                    
                     Cursor = Cursors.Hand
                 };
-
+                
+                
                 Label lbl = new Label
                 {
-                    Text = $"{entry.Data:dd/MM/yyyy HH:mm:ss}: {entry.Comando}",
+                    Text = $"{entry.Data:dd/MM/yyyy HH:mm:ss}: {entry.Comando} id: {entry.Id} qqqqqqqqqqqqqqqqqq qqqqqqqqqqqqqqqqq qqqqqqqqqqqqqqqqqqqq qqqqqqqqqqq qqqq qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",
                     Location = new Point(10, 15),
-                    AutoSize = true
+                   // AutoSize = false,
+                    Width = rectangleWidth -10 ,
+                    Height = rectangleHeight,
+                   
+
                 };
-                Label lbl2 = new Label
-                {
-                    Text = $"id: {entry.Id}",
-                    
-                    AutoSize = true
-                };
-                
+
+                int radius = 15;
+                entryPanel.Region = Region.FromHrgn(
+                    CreateRoundRectRgn(0, 0, entryPanel.Width, entryPanel.Height, radius, radius)
+                );
+
+
                 historyPanel.Controls.Add(entryPanel);
                 entryPanel.Controls.Add(lbl);
 
-                
+                entryPanel.SizeChanged += (s, e) => SetRoundedRegion(entryPanel, 15);
 
                 // Faz o hover bonito
                 void HoverEnter(object sender, EventArgs e) => entryPanel.BackColor = Color.SkyBlue;
@@ -123,9 +124,29 @@ namespace Vados
 
 
                 startY += rectangleHeight + margin;
+
+            }
+            
+
+            Panel spacer = new Panel
+            {
+                Location = new Point(0, startY-margin),
+                Size = new Size(rectangleWidth, spacing)
+            };
+
+            historyPanel.Controls.Add(spacer);
+        }
+
+        // arredonda as bordas das entradas
+        private void SetRoundedRegion(Control control, int radius)
+        {
+            if (control.Width > 0 && control.Height > 0)
+            {
+                control.Region = Region.FromHrgn(
+                    CreateRoundRectRgn(0, 0, control.Width, control.Height, radius, radius)
+                );
             }
         }
-       
 
         private void FormHistory_Resize_1(object sender, EventArgs e)
         {
